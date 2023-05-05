c9ab713ac031038fcbaf2aeed568acb06c68cb07
Jaisal Friedman <jaisal1024@gmail.com>
2023-05-05 17:52:05 -0400

    Add Revalidate, Update logs, add .nvmrc for my sanity (#1)

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,52 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { parseBody } from 'next-sanity/webhook';
+
+type Data = {
+  message: string;
+};
+type IncomingBody = {
+  type: string;
+  slug: string;
+};
+
+// Export the config from next-sanity to enable validating the request body signature properly
+export { config } from 'next-sanity/webhook';
+
+/*
+ * This endpoint is called by Sanity when a document is published. It only supports collections for now.
+ * See incremental ISR and Sanity webhooks - https://dev.to/valse/nextjs-on-demand-isr-by-sanity-groq-powered-webhooks-221n
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const { isValidSignature, body } = await parseBody(
+      req,
+      process.env.SANITY_WEBHOOK_SECRET
+    );
+
+    if (!isValidSignature) {
+      const message = 'Invalid signature';
+      console.warn(message);
+      res.status(401).json({ message });
+      return;
+    }
+
+    const { type, slug } = body as unknown as IncomingBody;
+
+    switch (type) {
+      case 'collections':
+        // always revalidate homepage too since it uses collections data
+        console.log(`Updating routes: ${type} with slug ${slug} and homepage`);
+        await res.revalidate(`/collections/${slug}`);
+        await res.revalidate('/');
+        return res.json({
+          message: `Revalidated "${type}" with slug "${slug}" and homepage`,
+        });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: err.message });
+  }
+}
