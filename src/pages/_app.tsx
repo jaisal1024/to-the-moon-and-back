598851a916fd9c2c9a08a022e8a9fc5154e884de
Jaisal Friedman <jaisal1024@gmail.com>
2024-10-01 23:20:52 -0400

    Setup GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,12 @@ import { ThemeProvider } from '@mui/material';
 import client from 'apollo-client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import PageTitle from 'src/components/PageTitle';
 import { theme } from 'src/theme';
+
+
+const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -22,6 +26,20 @@ export default function App({ Component, pageProps }: AppProps) {
               content="initial-scale=1.0, width=device-width"
             />
           </Head>
+          {/* Google tag (gtag.js) */}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+            async
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${googleAnalyticsId}');
+            `}
+          </Script>
           <Component {...pageProps} />
         </ApolloProvider>
       </ThemeProvider>
