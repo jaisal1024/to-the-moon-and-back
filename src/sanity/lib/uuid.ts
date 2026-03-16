f9235e30b594d3916559541492a60e51b533ffa8
Jaisal Friedman <jaisal1024@gmail.com>
2026-03-15 23:28:40 -0400

    chore: switch to bun, add knip

--- src/sanity/lib/uuid.ts
+++ src/sanity/lib/uuid.ts
@@ -1,9 +1,3 @@
-import nodeCrypto from 'crypto';
-
 export function createRandomUUID() {
-  if (typeof crypto !== 'undefined') {
-    return crypto.randomUUID();
-  }
-
-  return nodeCrypto.randomUUID();
+  return crypto.randomUUID();
 }
